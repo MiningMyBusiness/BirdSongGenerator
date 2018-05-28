9d5c8940aee077e3537d734143bde30958381a5d
Kiran Bhattacharyya <bhattacharyyakiran12@gmail.com>
2018-05-28 03:38:36 -0500

    Add files via upload

--- Code/CreateHMMSongModel.m
+++ Code/CreateHMMSongModel.m
@@ -1,12 +1,53 @@
+% NAME:
+% CreateHMMSongModel
+% Creates a certain number of user-defined songs for a virtual bird whose 
+%       songs are created by 
+%
+% REQUIRED INPUTS:
+% numOfSongs            the number of songs the user wants generated 
+% 
+%
+% OUTPUTS:
+% myFullSong            a waveform which contains all generated songs. 
+%
+% numOfHiddenStates     the number of hidden states in the HMM model used
+%                       to generate the syllable or chirp sequence 
+%
+% stateTransProb        the hidden state transition probability of the HMM
+%
+% stateTransCPD         the state transition CPD 
+%
+% obsStateProb          the observed state probability given a hidden state
+%
+% obsStateCPD           the observed state CPD 
+%
+% Fs                    the sampling frequency 
+%
+% sampMulti             multiplier slowing down playback frequency 
+%            
+% 
+% NOTE:
+% This function uses a data file call ChirpsByCluster.mat
+%
+% AUTHOR:
+% Kiran D. Bhattacharyya (bhattacharyykiran12@gmail.com)
+%
+% License: MIT License 
+%
+% Example use: 
+%           [myFullSong, numOfHiddenStates, stateTransProb, stateTransCPD,
+%           obsStateProb, obsStateCPD, Fs, sampMulti] = CreateHMMSongModel(5);
+%           sound(myFullSong, Fs/sampMulti); % play back generated songs 
+
 function [myFullSong, numOfHiddenStates, stateTransProb, stateTransCPD, obsStateProb, obsStateCPD, Fs, sampMulti] = CreateHMMSongModel(numOfSongs)
 
-load('ChirpsByCluster.mat')
+load('Data/ChirpsByCluster.mat')
 
 numOfClust = size(chirpsByCluster, 1);
 myIncr = 1/numOfClust;
 
 %% create random hidden markov model
-numOfHiddenStates = round(13*rand(1) + 3);
+numOfHiddenStates = round(13*rand(1) + 3); % generate number of hidden states 
 stateTransProb = rand(numOfHiddenStates);
 stateTransCPD = zeros(numOfHiddenStates);
 for ii = 1:numOfHiddenStates % get state transition probability
@@ -26,7 +67,7 @@ end
 obsStateProb = zeros(numOfHiddenStates, numOfClust); % probability of a chirp given a state 
 obsStateCPD = zeros(numOfHiddenStates, numOfClust); % cumulative probability at each chirp type 
 
-for ii = 1:numOfHiddenStates
+for ii = 1:numOfHiddenStates % generate observed state probability 
     obsStateProb(ii,:) = rand(1, numOfClust);
     if sum(obsStateProb(ii,:)) > 1
         valMulti = 1/sum(obsStateProb(ii,:));
@@ -41,17 +82,18 @@ songEndProb = betarnd(3,3); % probability that a song will end after a syllable,
 
 %% create parameters controlling chirp duration and playback rate
 Fs = 22050;
-sampMulti = rand(1)*2 + 1.3; % slow down playback rate 
-timeDur = 0.009*randn(1) + 0.24;
+sampMulti = rand(1)*2 + 1.3; % change the fundamental frequency of the chirps by slowing playback rate  
+timeDur = 0.009*randn(1) + 0.24; % time duration of a chirp 
 chirpDur = round(timeDur/(1/Fs));
 chirpDur_1 = round(chirpDur*0.95);
 chirpDur_2 = chirpDur - chirpDur_1;
-expDecay = exp(linspace(0, -6, chirpDur_2));
-myFullSong = 0;
+expDecay = exp(linspace(0, -6, chirpDur_2)); % add exponential decay to the end of chirps
+myFullSong = 0; % initialize a variale to store chirps 
 
-%% equally likely to start in any hidden state 
+%% create the songs using the randomly generated hidden markov model and the parameters
+%  equally likely to start in any hidden state 
 
-for songNum = 1:numOfSongs
+for songNum = 1:numOfSongs % for each song asked by the user to generate 
     currState = ceil(rand(1)/(1/numOfHiddenStates)); % intialize state 
 
     listOfStates = [currState, 0];
@@ -84,10 +126,10 @@ for songNum = 1:numOfSongs
             thisDecay = exp(linspace(0,-6, thisDur_2));
             thisChirp(thisDur_1:thisDur) = thisChirp(thisDur_1:thisDur).*[thisDecay 0];
         end
-        thisChirp = thisChirp*((randn(1)*0.2) + 1); % amplitude modulation to song to song variability 
+        thisChirp = thisChirp*((randn(1)*0.2) + 1); % amplitude modulation for song to song variability 
         % pitch modulation
         pitchMod = (rand(1)*0.07) + 1; % multiple of pitch
-        newLength = pitchMod*chirpDur;
+        newLength = pitchMod*chirpDur; % pitch modulate by resampling the signal 
         newChirp = spline(1:1:chirpDur, thisChirp, linspace(1, chirpDur, newLength));
         if newLength < chirpDur
             newChirp = newChirp + zeros(1, chirpDur - size(newChirp, 2));
@@ -95,40 +137,17 @@ for songNum = 1:numOfSongs
             newChirp = newChirp(1:chirpDur);
             newChirp(chirpDur_1:end) = newChirp(chirpDur_1:end).*[expDecay 0];
         end
-%         % chirp more broadband % bird sounds even more electronic 
-%         multiLo = randn(1)*0.1 + 0.5;
-%         multiHi = randn(1)*0.1 + 1.5;
-%         multiIncr = round(1*randn(1) + 10);
-%         multis = linspace(multiLo, multiHi, multiIncr);
-%         comboMultis = zeros(size(newChirp));
-%         for jj = 1:multiIncr
-%             subMulti = multis(jj);
-%             subLength = round(subMulti*chirpDur);
-%             subChirp = spline(1:1:chirpDur, newChirp, linspace(1, chirpDur, subLength));
-%             if subLength < chirpDur
-%                 comboMultis(1:subLength) = comboMultis(1:subLength) + subChirp;
-%             elseif subLength > chirpDur
-%                 comboMultis = comboMultis + subChirp(1:chirpDur);
-%             end
-%         end
-%         comboKiller = 10;
-%         newChirp = newChirp + (comboMultis/comboKiller);
-%         thisChirp = newChirp;
-        % add noise
-%         myNoise = wgn(size(thisChirp, 1), size(thisChirp, 2), 0.01);
-%         myNoise = myNoise/2000;
-%         thisChirp = thisChirp + myNoise;
-        flipChirp = rand(1) > betarnd(7,7);
+        flipChirp = rand(1) > betarnd(7,7); % flip the chirp with some probability 
         if flipChirp
             thisChirp = fliplr(thisChirp);
         end
         myFullSong = [myFullSong; thisChirp'];
     end
-
-    fillerLength = randn(1)*0.3 + 3;
-    filler = zeros(round(fillerLength*chirpDur), 1);
-%     filler = wgn(round(fillerLength*chirpDur), 1, 0.01);
-%     filler = filler/2000;
+    % add white noise the whole track to add "realness" of actual recording
+    myFullSong = myFullSong + (wgn(size(myFullSong, 1), size(myFullSong, 2), 0.01)/2000);
+    fillerLength = randn(1)*0.3 + 3; % define the silent space between songs 
+    filler = wgn(round(fillerLength*chirpDur), 1, 0.01); % create white noise for silent space 
+    filler = filler/2000;
     myFullSong = [myFullSong; filler];
 end
     
